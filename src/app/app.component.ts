dcbe1d6bef6128242b323e422e0cfdfb821bc943
Nagendra Kamisetti <nagendrakamisetti@gmail.com>
2021-08-18 01:44:10 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-forms';
+
+  @ViewChild('f')
+  signupForm: NgForm;
+
+  defaultQuestion: string = 'pet';
+  defaultUserName: string = 'sys user';
+  answer:string = '';
+  genders = ['male', 'female'];
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: 'test@gmail.com'
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+          username: suggestedName,
+      }
+    });
+  }
+
+
+  // onSubmit(form: HTMLFormElement) {
+  //   // console.log('on submit ', signupForm);
+  //   console.log('on submit ', form);
+  // }
+
+  onSubmit() {
+
+    console.log('on submit ', this.signupForm);
+  }
 }
